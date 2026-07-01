916870d1c09897ebf36cea6d8dc3ec36d7284724
Arpitha <sojnaarpitha@gmail.com>
2026-07-01 21:18:31 +0530

    Initial commit

---
+++ todo_list.py
@@ -0,0 +1,17 @@
+roadmap = [                                      #task ? task is variable 
+    "month 1: learn python basics",
+    "month 1: calculator project",
+    "month 1: Number guessing game ",
+    "month1: To do lists",
+    "month2: intermediate python and SQL basics",
+    "month3 : Git",
+    "month 3: Github",
+    "month 3: DSA basics",
+    "month 4 :HTML and CSS",
+    "month 4: Javascript",
+    "month 4: Flask",
+    "month 5 : Real projects",
+    "month 6 : job preparation", 
+]
+for  index, task in enumerate(roadmap, start =1):
+    print(index,".",task)
